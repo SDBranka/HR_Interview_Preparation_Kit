738b839f9f7078fc5c9e0a4ed1bb828537095647
Sam Branka <sdbranka@me.com>
2021-09-24 08:32:56 -0400

    #3 working local, but not on site

--- arrays.py
+++ arrays.py
@@ -65,11 +65,9 @@ import sys
 #     int arr[6][6]: an array of integers
 
 # Returns
-
 #     int: the maximum hourglass sum
 
 # Input Format
-
 # Each of the 6 lines of inputs arr[i] 
 # contains 6 space-separated integers arr[i][j].
 
@@ -78,12 +76,10 @@ import sys
     # 0 <= i, j <= 5
 
 # Output Format
-
 # Print the largest (maximum) hourglass 
 # sum found in arr.
 
 # Sample Input
-
 # 1 1 1 0 0 0
 # 0 1 0 0 0 0
 # 1 1 1 0 0 0
@@ -92,11 +88,9 @@ import sys
 # 0 0 1 2 4 0
 
 # Sample Output
-
 # 19
 
 # Explanation
-
 # contains the following hourglasses:
 
 # 111 110 100 000
@@ -116,17 +110,15 @@ import sys
 # 001 012 124 240
 
 # The hourglass with the maximum sum (19) is:
-
 # 2 4 4
 #   2
 # 1 2 4
 
 # Complete the 'hourglassSum' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts 2D_INTEGER_ARRAY arr as parameter.
 #
-
+# # as a 1d array
 # def hourglassSum(arr):
 #     # Write your code here
 #     max_sum = 0
@@ -193,20 +185,16 @@ arr = [[-1, -1, 0, -9, -2, -2],
 # line of space-separated integers.
 
 # Function Description
-
 # Complete the function rotLeft in the editor below.
 
 # rotLeft has the following parameter(s):
-
 #     int a[n]: the array to rotate
 #     int d: the number of rotations
 
 # Returns
-
 #     int a'[n]: the rotated array
 
 # Input Format
-
 # The first line contains two space-separated integers
 # n and d, the size of a and the number of left 
 # rotations.
@@ -219,16 +207,13 @@ arr = [[-1, -1, 0, -9, -2, -2],
 # 1 <= a[i] <= 10^6
 
 # Sample Input
-
 # 5 4
 # 1 2 3 4 5
 
 # Sample Output
-
 # 5 1 2 3 4
 
 # Explanation
-
 # When we perform d = 4 left rotations, the array
 # undergoes the following sequence of changes:
 # [1, 2, 3, 4, 5] ->
@@ -252,20 +237,160 @@ def rotLeft(a, d):
     # return a
 
     n = len(a)
-    a[:]=a[d:n]+a[0:d]
+    a[:] = a[d:n] + a[0:d]
     return a
 
 
+# Sample Input
+d = 4
+a = [1, 2, 3, 4, 5]
+# Sample Output
+# 5 1 2 3 4
 
+# print(rotLeft(a, d))
 
+# # 3 New Year Chaos
+# It is New Year's Day and people are in line for the 
+# Wonderland rollercoaster ride. Each person wears a 
+# sticker indicating their initial position in the queue 
+# from 1 to n. Any person can bribe the person directly 
+# in front of them to swap positions, but they still wear 
+# their original sticker. One person can bribe at most 
+# two others.
 
+# Determine the minimum number of bribes that took 
+# place to get to a given queue order. Print the number 
+# of bribes, or, if anyone has bribed more than two people,
+#  print Too chaotic.
+
+# Example
+# q ={1, 2, 3, 5, 4, 6, 7, 8}
+# If person 5 bribes person 4, the queue will look 
+# like this: 1, 2, 3, 5, 4, 6, 7, 8. Only 1bribe is 
+# required. Print 1.
+
+# q = [4, 1, 2, 3 ]
+# Person 4 had to bribe 3 people to get to the 
+# current position. Print Too chaotic.
+
+# Function Description
+# Complete the function minimumBribes in the editor below.
+
+# minimumBribes has the following parameter(s):
+#     int q[n]: the positions of the people after all bribes
+
+# Returns
+#     No value is returned. Print the minimum number of
+#     bribes necessary or Too chaotic if someone has bribed 
+#     more than 2 people.
+
+# Input Format
+# The first line contains an integer t, the number of 
+# test cases.
+
+# Each of the next t pairs of lines are as follows:
+# - The first line contains an integer t, the number of 
+#   people in the queue
+# - The second line has n space-separated integers 
+#   describing the final state of the queue.
+
+# Constraints
+# 1 <= t <= 10
+# 1 <= n <= 10^5
+
+# Subtasks
+
+# For 60% score 1 <= n <= 10^3 
+# For 100% score 1 <= n <= 10^5
 
 # Sample Input
 
-d = 4
-a = [1, 2, 3, 4, 5]
+# STDIN       Function
+# -----       --------
+# 2           t = 2
+# 5           n = 5
+# 2 1 5 3 4   q = [2, 1, 5, 3, 4]
+# 5           n = 5
+# 2 5 1 3 4   q = [2, 5, 1, 3, 4]
 
 # Sample Output
 
-# 5 1 2 3 4
-print(rotLeft(a, d))
+# 3
+# Too chaotic
+
+# Explanation
+
+# Test Case 1
+
+# The initial state:
+# Ride- 1 2 3 4 5
+
+# After person 5 moves one position ahead by bribing person 4:
+# Ride- 1 2 3 5 4
+
+# Now person 5 moves another position ahead by bribing person 3:
+# Ride- 1 2 5 3 4
+
+# And person 2 moves one position ahead by bribing person 1:
+# Ride- 2 1 5 3 4
+
+# So the final state is 2, 1, 5, 3, 4 after three
+#  bribing operations.
+
+# Test Case 2
+
+# No person can bribe more than two people, yet it appears 
+# person 5 has done so. It is not possible to achieve 
+# the input state.
+
+
+
+def minimumBribes(q):
+    num_of_moves = 0
+    for x in range(len(q)):
+        if q[x] - 1 > 2 + x:
+            return "Too chaotic"
+
+        for j in range(max(0, q[x]-2), x):
+            if q[j] > q[x]:
+                num_of_moves += 1
+
+    return num_of_moves
+
+q = [1, 2, 3, 5, 4, 6, 7, 8]
+# Output
+# 1
+
+# q = [4, 1, 2, 3 ]
+# Output
+# Too chaotic.
+
+# qt = [1, 0, 4, 2, 3]
+# q = [2, 1, 5, 3, 4]
+# Output
+# 3
+
+# q = [2, 5, 1, 3, 4]
+# Output
+# Too chaotic
+
+# q = [5, 1, 2, 3, 7, 8, 6, 4]
+# Output
+# Too chaotic
+
+# r = [0, 1, 4, 2, 6, 7, 5, 3]
+# q = [1, 2, 5, 3, 7, 8, 6, 4]
+# Output
+# 7
+
+# q = [1, 2, 5, 3, 4, 7, 8, 6]
+# Output
+# 4
+
+# q = [2, 3, 5, 4, 6, 7, 8, 1]
+# Output
+# 
+
+
+print(minimumBribes(q))
+
--- warm_up_challenges.py
+++ warm_up_challenges.py
@@ -11,7 +11,6 @@ import sys
 # determine how many pairs of socks with matching colors there are.
 
 # Example
-
 # n = 7
 # ar = [1, 2, 1, 2, 1, 3, 2]
 
@@ -19,38 +18,31 @@ import sys
 # odd socks left, one of each color. The number of pairs is 2.
 
 # Function Description
-
 # Complete the sockMerchant function in the editor below.
 
 # sockMerchant has the following parameter(s):
-
 #     -int n: the number of socks in the pile
 #     -int ar[n]: the colors of each sock
 
 # Returns
-
 #     -int: the number of pairs
 
 # Input Format
-
 # The first line contains an integer n, the number of socks represented 
 # in ar. The second line contains n space-separated integers, ar[i], 
 # the colors of the socks in the pile.
 
 # Constraints
-
 # -1 <= n <= 100
 # 1 <= ar[i] <= 100 where 0 <= i <= n
 
 # Sample Input
-
 # STDIN                       Function
 # -----                       --------
 # 9                           n = 9
 # 10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
 
 # Sample Output
-
 # 3
 
 # Example
@@ -66,7 +58,6 @@ import sys
 
 
 # Complete the 'sockMerchant' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts following parameters:
 #  1. INTEGER n
@@ -247,7 +238,6 @@ path = "DDUUUUDD"
 # - c[0] = c[n-1] = 0
 
 # Output Format
-
 # Print the minimum number of jumps needed to win the game.
 
 # Sample Input 0
@@ -413,8 +403,6 @@ def repeatedString(s, n):
                 if s[letter] == 'a':
                     count += 1
     return count
-
-
 
 
 # Input:
